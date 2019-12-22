f7047aa3ad870541f41dd5c99bc149b04f888a95
Shusei Ichinomiya <32806850+itisyuu@users.noreply.github.com>
2019-12-23 07:43:19 +0900

    Create A - Round One.cs

---
+++ AtCoder Beginner Contest 148/A - Round One.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+namespace AtCoder
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var a = 6;
+            var N = int.Parse(Console.ReadLine());
+            var N1 = int.Parse(Console.ReadLine());
+            Console.WriteLine(a - N - N1);
+        }
+        
+    }
+}
