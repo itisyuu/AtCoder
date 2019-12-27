2012bf18d6ae431782cc2b1aecb08281f07db19a
Shusei Ichinomiya <32806850+itisyuu@users.noreply.github.com>
2019-12-28 04:06:48 +0900

    Create A - Circle.cs

---
+++ AtCoder Beginner Contest 145/A - Circle.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+namespace AtCoder
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var N = int.Parse(Console.ReadLine());
+            Console.Write(N*N);
+        }
+    }
+}
